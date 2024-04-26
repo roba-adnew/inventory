ef53943cad780c585a0344b253e900e406c4dcd9
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-26 10:54:36 -0400

    Re-implkemented home page with a dynamic asyncHandler

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,55 +3,65 @@ const Category = require('../models/category')
 
 const asyncHandler = require('express-async-handler');
 
-exports.index = asyncHandler(async (req, res) => {
-    const [numProducts, numCategories] =
-        await Promise.all([
-            Product.countDocuments({}).exec(),
-            Category.countDocuments({}).exec()
-        ]);
-        res.render('layout', {
-            title: 'Diglets Department Store',
-            productCount: numProducts,
-            categoryCount: numCategories
-        })
-});
-
-// Display list of all products
-exports.productList = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product list')
+exports.dynamicHandler = asyncHandler(async (req, res) => {
+    switch (req.path) {
+        case '/':
+            const [numProducts, numCategories] =
+                await Promise.all([
+                    Product.countDocuments({}).exec(),
+                    Category.countDocuments({}).exec()
+                ]);
+            res.render('layout', {
+                title: 'Diglets Department Store',
+                productCount: numProducts,
+                categoryCount: numCategories
+            });
+        
+        
+    }
 })
 
-// Display detail page for a given product
-exports.productDetail = asyncHandler(async (req, res) => {
-    res.send(`NOT IMPLEMENTED: Product detail: ${req.params.id}`)
-})
 
-// Display product create form on GET
-exports.productCreateGet = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product create GET')
-})
 
-// Display product create form on POST
-exports.productCreatePost = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product create POST')
-})
 
-// Display product delete form on GET
-exports.productDeleteGet = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product delete GET')
-})
+// // Display list of all products
+// exports.productList = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product list')
+// })
 
-// Display product delete form on POST
-exports.productDeletePost = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product delete POST')
-})
+// // Display detail page for a given product
+// exports.productDetail = asyncHandler(async (req, res) => {
+//     res.send(`NOT IMPLEMENTED: Product detail: ${req.params.id}`)
+// })
 
-// Display product update form on GET
-exports.productUpdateGet = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product update GET')
-})
+// // Display product create form on GET
+// exports.productCreateGet = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product create GET')
+// })
 
-// Display product update form on POST
-exports.productUpdatePost = asyncHandler(async (req, res) => {
-    res.send('NOT IMPLEMENTED: Product update POST')
-})
+// // Display product create form on POST
+// exports.productCreatePost = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product create POST')
+// })
+
+// // Display product delete form on GET
+// exports.productDeleteGet = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product delete GET')
+// })
+
+// // Display product delete form on POST
+// exports.productDeletePost = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product delete POST')
+// })
+
+// // Display product update form on GET
+// exports.productUpdateGet = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product update GET')
+// })
+
+// // Display product update form on POST
+// exports.productUpdatePost = asyncHandler(async (req, res) => {
+//     res.send('NOT IMPLEMENTED: Product update POST')
+// })
+
+// module.exports = dynamicHandler;
--- routes/catalog.js
+++ routes/catalog.js
@@ -7,56 +7,61 @@ const productController = require("../controllers/productController");
 /// PRODUCT ROUTES ///
 
 // GET catalog home page.
-router.get("/", productController.index);
 
-// GET request for creating a Product. NOTE This must come before routes that display Product (uses id).
-router.get("/product/create", productController.productCreateGet);
+router.get('*', productController.dynamicHandler)
 
-// POST request for creating Product.
-router.post("/product/create", productController.productCreatePost);
+// // GET request for list of all Product items.
+// router.get("/products", productController.productList);
 
-// GET request to delete Product.
-router.get("/product/:id/delete", productController.productDeleteGet);
 
-// POST request to delete Product.
-router.post("/product/:id/delete", productController.productDeletePost);
 
-// GET request to update Product.
-router.get("/product/:id/update", productController.productUpdateGet);
 
-// POST request to update Product.
-router.post("/product/:id/update", productController.productUpdatePost);
+// // GET request for creating a Product. NOTE This must come before routes that display Product (uses id).
+// router.get("/product/create", productController.productCreateGet);
 
-// GET request for one Product.
-router.get("/product/:id", productController.productDetail);
+// // POST request for creating Product.
+// router.post("/product/create", productController.productCreatePost);
 
-// GET request for list of all Product items.
-router.get("/products", productController.productList);
+// // GET request to delete Product.
+// router.get("/product/:id/delete", productController.productDeleteGet);
 
-/// CATEGORY ROUTES ///
+// // POST request to delete Product.
+// router.post("/product/:id/delete", productController.productDeletePost);
 
-// GET request for creating a Category. NOTE This must come before routes that display Category (uses id).
-router.get("/category/create", categoryController.categoryCreateGet);
+// // GET request to update Product.
+// router.get("/product/:id/update", productController.productUpdateGet);
 
-// POST request for creating Category.
-router.post("/category/create", categoryController.categoryCreatePost);
+// // POST request to update Product.
+// router.post("/product/:id/update", productController.productUpdatePost);
 
-// GET request to delete Category.
-router.get("/category/:id/delete", categoryController.categoryDeleteGet);
+// // GET request for one Product.
+// router.get("/product/:id", productController.productDetail);
 
-// POST request to delete Category.
-router.post("/category/:id/delete", categoryController.categoryDeletePost);
 
-// GET request to update Category.
-router.get("/category/:id/update", categoryController.categoryUpdateGet);
+// /// CATEGORY ROUTES ///
 
-// POST request to update Category.
-router.post("/category/:id/update", categoryController.categoryUpdatePost);
+// // GET request for creating a Category. NOTE This must come before routes that display Category (uses id).
+// router.get("/category/create", categoryController.categoryCreateGet);
 
-// GET request for one Category.
-router.get("/category/:id", categoryController.categoryDetail);
+// // POST request for creating Category.
+// router.post("/category/create", categoryController.categoryCreatePost);
 
-// GET request for list of all Category items.
-router.get("/categories", categoryController.categoryList);
+// // GET request to delete Category.
+// router.get("/category/:id/delete", categoryController.categoryDeleteGet);
+
+// // POST request to delete Category.
+// router.post("/category/:id/delete", categoryController.categoryDeletePost);
+
+// // GET request to update Category.
+// router.get("/category/:id/update", categoryController.categoryUpdateGet);
+
+// // POST request to update Category.
+// router.post("/category/:id/update", categoryController.categoryUpdatePost);
+
+// // GET request for one Category.
+// router.get("/category/:id", categoryController.categoryDetail);
+
+// // GET request for list of all Category items.
+// router.get("/categories", categoryController.categoryList);
 
 module.exports = router;
