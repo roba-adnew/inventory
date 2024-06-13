10cea30b1d54e5ace83f5501dcffafd513fa48ad
roba <10408936+roba-adnew@users.noreply.github.com>
2024-06-13 13:16:44 -0400

    Moved authentication codeblocks to route file

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-// var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 const compression = require("compression");
 const helmet = require("helmet");
-const User = require('./models/user')
-const bcrypt = require('bcryptjs')
 require('dotenv').config();
 
 var app = express();
@@ -58,49 +54,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-// passport set-up
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      };
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" });
-      };
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    };
-  })
-);
-
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  };
-});
-
-app.post(
-  "/users/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/"
-  })
-);
 
 app.get("/users/log-out", (req, res, next) => {
   req.logout((err) => {
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('../models/user');
+const bcrypt = require('bcryptjs')
+
+
+
 const accountController = require("../controllers/accountController");
 
-/* NEW USER CREATION GET AND POST */
+/* NEW USER CREATION GET & POST */
 router.get("/sign-up", accountController.accountCreateGet)
 
 router.post("/sign-up", accountController.accountCreatePost)
 
-/* NEW USER CREATION GET AND POST */
+/* LOGIN GET & POST */
 router.get("/login", accountController.loginGet)
+
+// passport set-up
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        };
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        };
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      };
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    };
+  });
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  }))
 
 module.exports = router;
